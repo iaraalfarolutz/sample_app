81231b219ac378a73c694f456a66c9224710fc1f
Iara Alfaro Lutz <iaraalfarolutz@gmail.com>
2017-12-29 19:52:22 +0000

    Sign-up for ready

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user, options = { size: 112 })  #(user, size: 93) son lo mesmo pero este tiene tam estatico
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size] #si uso es otro tengo que sacar esto
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :name, presence: true,  length: { maximum: 50 } # es un metodo validates, que tiene como parametros el simbolo name y presence con valor true
   validates :email, presence: true,  length: { maximum: 255 }, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 
 end
