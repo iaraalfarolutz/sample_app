5ca518c6cf05e86e0eb0c4fb95d9c385de343e79
Iara Alfaro Lutz <iaraalfarolutz@gmail.com>
2018-01-15 13:24:31 +0000

    Finish mircoposts feed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -85,4 +85,32 @@ test "email validation should accept valid addresses" do
     end
   end
   
+  test "should follow and unfollow a user" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
+  
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # Posts from self
+    michael.microposts.each do |post_self|    #por cada micropost de cada uno me fijo si aparece en el feed de michael, archer no tiene que estar
+      assert michael.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
 end
